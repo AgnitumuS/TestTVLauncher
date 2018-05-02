47fbbae5da763faa2e82684e5af8e3f93f188db7
xiaoqi <xiaoqi@isec-tech.com>
2018-05-02 15:07:21 +0800

    TV测试项目初步搭建

---
+++ app/src/main/java/com/explame/testtvlauncher/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.explame.testtvlauncher.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.explame.testtvlauncher.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
